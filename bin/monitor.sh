a4d56d57d27b46075ee2b1ede928467be0a6b55a
Donghua.Lau <me@liudonghua.net>
2019-09-12 07:48:34 +0800

    update

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -13,11 +13,14 @@ function monitor()
 	cmd=$2
 	pid=`ps hf -opid -C "${name}" | awk '{print $1; exit }'`
 	if [ "${pid}SS" == "SS" ]; then
+		echo "'${name}' not found and to start"
 		proc_start "${name}" "${cmd}"
 	fi
 }
 
 # (full process name, start command) 
-monitor "httpd" "service httpd start"
+monitor "mysqld" "/sbin/service mariadb start"
+monitor "httpd" "/sbin/service httpd start"
+monitor "traffic_cop" "/usr/local/ats/bin/trafficserver start"
 
 

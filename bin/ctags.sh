7f3fa929c900b5c976c911a9ec94eb2f1c07f6e2
Donghua.Lau <me@liudonghua.net>
2020-03-26 14:54:38 +0800

    update scripts

--- bin/ctags.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# ctags -R --c++-kinds=+p --fields=+iaS --extra=+q /usr/include
-# ctags -R --c-kinds=+p --language-force=C .
---
+++ linux/bin/monitor.sh
@@ -0,0 +1,1 @@
+/root/projects/linux-custom/bin/monitor.sh
